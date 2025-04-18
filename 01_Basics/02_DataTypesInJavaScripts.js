313f352834b83fc35b4dc3ac70e0ba6db425ef65
Sachinkumar9458 <149507876+Sachinkumar9458@users.noreply.github.com>
2025-04-18 06:36:35 +0000

    Stack and heap concept

--- 01_Basics/02_DataTypesInJavaScripts.js
+++ 01_Basics/02_DataTypesInJavaScripts.js
@@ -21,48 +21,70 @@
 // - JSON
 // - Error
 // 1. Primitive Data Types
-let name = "John Doe"; // String
-let age = 25; // Number
-let isMarried = false; // Boolean
-let address; // Undefined
-let salary = null; // Null
-let uniqueId = Symbol("id"); // Symbol is used for uniqueness.
-let bigNumber = BigInt(123456789012345678901234567890); // BigInt
-// 2. Non-Primitive Data Types
-let person = { // Object
-    name: "John Doe",
-    age: 25,
-    isMarried: false
-};
-let numbers = [1, 2, 3, 4, 5]; // Array
-let greet = function() { // Function
-    console.log("Hello World");
-};
-let date = new Date(); // Date
-let regex = /[a-z]/; // RegExp
-let map = new Map(); // Map
-let set = new Set(); // Set
-let weakMap = new WeakMap(); // WeakMap
-let weakSet = new WeakSet(); // WeakSet
-let json = JSON.stringify(person); // JSON
-let error = new Error("This is an error"); // Error
-// Type Checking
-console.log(typeof name); // string
-console.log(typeof age); // number
-console.log(typeof isMarried); // boolean
-console.log(typeof address); // undefined
-console.log(typeof salary); // object
-console.log(typeof uniqueId); // symbol
-console.log(typeof bigNumber); // bigint
-console.log(typeof person); // object
-console.log(typeof numbers); // object
-console.log(typeof greet); // function
-console.log(typeof date); // object
-console.log(typeof regex); // object
-console.log(typeof map); // object
-console.log(typeof set); // object
-console.log(typeof weakMap); // object
-console.log(typeof weakSet); // object
-console.log(typeof json); // string
-console.log(typeof error); // object
+// let name = "John Doe"; // String
+// let age = 25; // Number
+// let isMarried = false; // Boolean
+// let address; // Undefined
+// let salary = null; // Null
+// let uniqueId = Symbol("id"); // Symbol is used for uniqueness.
+// let bigNumber = BigInt(123456789012345678901234567890); // BigInt
+// // 2. Non-Primitive Data Types
+// let person = { // Object
+//     name: "John Doe",
+//     age: 25,
+//     isMarried: false
+// };
+// let numbers = [1, 2, 3, 4, 5]; // Array
+// let greet = function() { // Function
+//     console.log("Hello World");
+// };
+// let date = new Date(); // Date
+// let regex = /[a-z]/; // RegExp
+// let map = new Map(); // Map
+// let set = new Set(); // Set
+// let weakMap = new WeakMap(); // WeakMap
+// let weakSet = new WeakSet(); // WeakSet
+// let json = JSON.stringify(person); // JSON
+// let error = new Error("This is an error"); // Error
+// // Type Checking
+// console.log(typeof name); // string
+// console.log(typeof age); // number
+// console.log(typeof isMarried); // boolean
+// console.log(typeof address); // undefined
+// console.log(typeof salary); // object
+// console.log(typeof uniqueId); // symbol
+// console.log(typeof bigNumber); // bigint
+// console.log(typeof person); // object
+// console.log(typeof numbers); // object
+// console.log(typeof greet); // function
+// console.log(typeof date); // object
+// console.log(typeof regex); // object
+// console.log(typeof map); // object
+// console.log(typeof set); // object
+// console.log(typeof weakMap); // object
+// console.log(typeof weakSet); // object
+// console.log(typeof json); // string
+// console.log(typeof error); // object
 
+
+// .....................stack    and  heap.................
+//primitive(stack (copy)) and nonprimitive(heap(referance))
+
+// let myName = "sachin kumar";
+// let anotherName = myName;
+// anotherName = "ankit";
+
+
+// console.log(anotherName)
+// console.log(myName)
+
+let userone={
+    email: "sdjasidcna@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userone;
+userTwo.email = "sachin@gmail.com";
+
+console.log(userone.email);
+console.log(userTwo.email);
