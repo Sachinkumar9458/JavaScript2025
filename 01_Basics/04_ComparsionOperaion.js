c02e2b27f8d35b32140cffc19cb243f200078df4
Sachinkumar9458 <149507876+Sachinkumar9458@users.noreply.github.com>
2025-04-18 05:51:59 +0000

    something more about the comparsion operator

--- 01_Basics/04_ComparsionOperaion.js
+++ 01_Basics/04_ComparsionOperaion.js
@@ -1,7 +1,47 @@
 //Comparsion oparation
-console.log(2<29);
-console.log(3>39);
-console.log(39>=34);
-console.log(39<=84);
-console.log(3!=39);
+// console.log(2<29);
+// console.log(3>39);
+// console.log(39>=34);
+// console.log(39<=84);
+// console.log(3!=39);
+
+// console.log("3">0)
+// console.log("03">0)
+
+// console.log(null>0)
+// console.log(null<0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+// strict check(===)
+
+console.log("3"===3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
