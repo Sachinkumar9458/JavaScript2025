7d9839d4988a6a0d8d0fb422f51f7792d9c0eeeb
Sachinkumar9458 <149507876+Sachinkumar9458@users.noreply.github.com>
2025-05-06 09:44:19 +0000

    arrays part_2

---
+++ 02_Basics/02_arrays.js
@@ -0,0 +1,31 @@
+// // const marvel_heroes = ["Thor", "Ironman", "Hulk", "Captain America", "Black Widow"];
+// // const indian_heroes = ["Shaktimaan", "Krrish", "Ra.One", "G.One", "Drona"];
+
+// // marvel_heroes.push("Doctor Strange");
+// // // console.log(marvel_heroes);
+
+// // // marvel_heroes.push(indian_heroes);
+// // // console.log(marvel_heroes);
+// // // console.log(marvel_heroes.length); // 6
+// // // console.log(marvel_heroes[5][3]); // [ 'Shaktimaan', 'Krrish', 'Ra.One', 'G.One', 'Drona' ]
+// // console.log(marvel_heroes.concat(indian_heroes));
+
+
+// // console.log(all_newHeros);
+
+// let another_array = [1,2,3,[4,5,6,[7,8,9]]];
+// // console.log(another_array.flat(Infinity)); // [1, 2, 3, 4, 5, 6, 7, 8, 9] // Flatten the array
+// const rearl_array = another_array.flat(2); // [1, 2, 3, 4, 5, 6, 7, 8, 9] // Flatten the array
+// console.log(rearl_array); // [1, 2, 3, 4, 5, 6, 7, 8, 9] // Flatten the array
+// // console.log(another_array.flat(2)); // [1, 2, 3, 4, 5, 6, [7, 8, 9]] // Flatten the array
+
+// let name = "Shivam";
+// console.log(Array.isArray(name)); // false
+
+// console.log(Array.from(name)); // [ 'S', 'h', 'i', 'v', 'a', 'm' ] // Convert string to array
+// console.log(Array.isArray(name));
+
+let arr1 = 100;
+let arr2 = 200;
+let arr3 = 300;
+console.log(Array.of(arr1,arr2,arr3)); // 100 200 300
